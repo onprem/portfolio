335149d2e40c3702afbf4c46538d575c2461bcba
Prem Kumar <prmsrswt@gmail.com>
2019-09-03 19:56:22 +0530

    initialised react

--- js/script.js
+++
@@ -1,6 +0,0 @@
-var burgers = document.getElementsByClassName('burgers')[0];
-var labeel = document.getElementById('labeel');
-burgers.addEventListener('click', () => burgers.classList.toggle('open'));
-burgers.addEventListener('click', () => document.getElementsByClassName('container')[0].classList.toggle('inverti'));
-// labeel.addEventListener('click', () => burgers.classList.toggle('open'));
-// labeel.addEventListener('click', () => document.getElementById('menu').classList.toggle('shownav'));
