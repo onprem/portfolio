cf9622623ca84b17f6b8c2b20478fbd8ac1d2b20
Prem Kumar <prmsrswt@gmail.com>
2020-03-31 04:31:11 +0530

    setup routing; minor styling fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 import Home from './components/Home/Home';
 import './App.css';
@@ -7,7 +8,12 @@ function App() {
   return (
     <div className="App">
       <Nav />
-      <Home />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+      
     </div>
   );
 }
